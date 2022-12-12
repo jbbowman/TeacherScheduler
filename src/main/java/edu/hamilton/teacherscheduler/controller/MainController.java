6b07f12c63e98472a619b1da6e0b544e76b929ed
Zach <zmlinebaugh@student.unwsp.edu>
2022-12-12 16:41:12 -0600

    Everything should be working, but I cannot figure out why I get an error when I try to switch scenes.

--- src/main/java/edu/hamilton/teacherscheduler/controller/MainController.java
+++ src/main/java/edu/hamilton/teacherscheduler/controller/MainController.java
@@ -1,19 +1,19 @@
 package edu.hamilton.teacherscheduler.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.*;
 import javafx.scene.*;
-import javafx.fxml.FXMLLoader;
+
 import java.io.IOException;
 
 public class MainController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     public void switchToSettings(ActionEvent event) throws IOException {
-            Parent root = FXMLLoader.load(getClass().getResource("settings-view.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../../../../../resources/edu/hamilton/teacherscheduler/settings-view.fxml"));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
