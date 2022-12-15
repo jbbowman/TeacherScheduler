b26f660560875e9d277c1e8c3bb5bd30a1b16539
Zach <zmlinebaugh@student.unwsp.edu>
2022-12-14 20:20:42 -0600

    Progress

--- src/main/java/edu/hamilton/teacherscheduler/controller/MainController.java
+++ src/main/java/edu/hamilton/teacherscheduler/controller/MainController.java
@@ -11,31 +11,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainController {
-    @FXML
-    private Pane rootPane;
-    String[] fileNames = {"schedule-view.fxml", "settings-view.fxml"};
-    @FXML
-    public void initialize() {changeIndex(fileNames, 0);}
-    @FXML
-    private void handleHomeB(ActionEvent event) {changeIndex(fileNames, 0);}
-    @FXML
-    private void handleScheduleB(ActionEvent event) {changeIndex(fileNames, 1);}
-    @FXML
-    private void handleSpecialsB(ActionEvent event) {changeIndex(fileNames, 2);}
-    @FXML
-    private void handleTeachersB(ActionEvent event) {changeIndex(fileNames, 3);}
-    @FXML
-    private void handleSettingsB(ActionEvent event) {changeIndex(fileNames, 4);}
+        @FXML
+        private Pane rootPane;
+        String[] fileNames = {"schedule-view.fxml", "settings-view.fxml", "specials-view.fxml", "teacher-view2.fxml"};
+        @FXML
+        public void initialize () {
+       changeIndex(fileNames, 0);
+    }
+        @FXML
+        private void handleHomeB (ActionEvent event){
+        changeIndex(fileNames, 0);
+    }
+        @FXML
+        private void handleScheduleB (ActionEvent event){
+        changeIndex(fileNames, 1);
+    }
+        @FXML
+        private void handleSpecialsB (ActionEvent event){
+        changeIndex(fileNames, 2);
+    }
+        @FXML
+        private void handleTeachersB (ActionEvent event){
+        changeIndex(fileNames, 3);
+    }
+        @FXML
+        private void handleSettingsB (ActionEvent event){
+        changeIndex(fileNames, 4);
+    }
 
-    private void changeIndex(String[] fileNames, int index) {
+        private void changeIndex (String[]fileNames,int index){
         try {
             List<Pane> array = loadPages(fileNames);
             rootPane.getChildren().clear();
             rootPane.getChildren().add(array.get(index));
-        } catch (IOException e) {e.printStackTrace();}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private List<Pane> loadPages(String[] fileNames) throws IOException {
+        private List<Pane> loadPages (String[]fileNames) throws IOException {
         List<Pane> pages = new ArrayList<>();
 
         for (String fileName : fileNames) {
@@ -44,4 +58,5 @@ public class MainController {
         }
         return pages;
     }
-}
+    }
+
